4a4b9ae4c6037b812d3c5cf432907ae949187309
Ronie - MIADP <ronie.miadp@gmail.com>
2025-09-02 11:55:58 +0800

    updated with prisma

--- actions/plant.action.ts
+++ actions/plant.action.ts
@@ -3,6 +3,7 @@
 import { prisma } from "@/lib/prisma";
 import { getUserId } from "./user.action";
 import { revalidatePath } from "next/cache";
+import { Prisma } from "@prisma/client";
 
 export async function getPlants(searchTerm?: string){
     try {
@@ -33,5 +34,52 @@ export async function getPlantById(id: string){
         return result;
     } catch (error) {
         console.info(`Error in getPlantById`, error);
+    }
+}
+
+export async function createPlant(data: Prisma.PlantsCreateInput){
+    console.info(data);
+    try {
+        const currentUserId = await getUserId();
+        if(!currentUserId) return;
+
+        const newPlant = await prisma.plants.create({
+            data: {...data, userId: currentUserId }
+        });
+        revalidatePath("/plants");
+        return newPlant;
+    } catch (error) {
+        console.error("Error Creating Plant:", error);
+        throw error;
+    }
+}
+
+export async function editPlant(id: string, data: Prisma.PlantsUpdateInput){
+    try {
+        const currentUserId = await getUserId();
+        if(!currentUserId) return;
+
+        const updatedPlant = await prisma.plants.update({ 
+            where: { id }, data: { ...data, userId: currentUserId } 
+        });
+        revalidatePath("/plants");
+        return updatedPlant;
+    } catch (error) {
+        console.error("Error updating Plant:", error);
+        throw error;
+    }
+}
+
+export async function deletePlant(id: string){
+    try {    
+        const currentUserId = await getUserId();
+        if(!currentUserId) return; 
+
+        const deletedPlant = await prisma.plants.delete({ where: { id } });
+        revalidatePath("/plants");
+        return deletedPlant;
+    } catch (error) {
+        console.error("Error deleting Plant:", error);
+        throw error;
     }
 }
